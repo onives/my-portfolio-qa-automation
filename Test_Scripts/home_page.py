785f53d78322c6e35e338cb41798f2ce576d9fc0
Olive Nives Nameere <olivenives63@gmail.com>
2022-03-23 22:33:39 +0300

    added another test

--- Test_Scripts/home_page.py
+++ Test_Scripts/home_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 import pdb
 import time
 import unittest
+import pdb
 
 
 class MyPortfolioTestScript(unittest.TestCase):
@@ -23,10 +24,17 @@ class MyPortfolioTestScript(unittest.TestCase):
         # verify the url of the page
         current_url = self.driver.current_url
         expected_url = "https://nameere-olive-nives.netlify.app/"
-        self.assertEqual(expected_url, current_url, f"Got wrong title. Current title: {current_url}")
+        self.assertEqual(expected_url, current_url, f"Got wrong site url. Current url: {current_url}")
 
-        time.sleep(5)
+        # verify the content of the bio section of the home-page is displayed
+        bio_link_locator = "#root > div > div.container-div > div.bio-div > p"
+        bio_element = self.driver.find_element(By.CSS_SELECTOR, bio_link_locator)
+        # wait a few seconds before checking since bio api takes long to load on browser
+        time.sleep(2)
+        self.assertTrue(bio_element.is_displayed())
 
+        # pdb.set_trace()
+        # time.sleep(5)
     def tearDown(self) -> None:
         self.driver.quit()
 
