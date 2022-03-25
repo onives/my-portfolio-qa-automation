da8f5178c878bb600ab1fcd965f354196fbb4837
Olive Nives Nameere <olivenives63@gmail.com>
2022-03-25 23:33:51 +0300

    adding test case for projects page

--- Test_Scripts/projects_page.py
+++ Test_Scripts/projects_page.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import pdb
 import time
 import unittest
-import pdb
 
 
 class MyPortfolioTestScript(unittest.TestCase):
@@ -12,26 +12,25 @@ class MyPortfolioTestScript(unittest.TestCase):
         self.driver = webdriver.Firefox()
         self.driver.get("https://nameere-olive-nives.netlify.app/")
 
-    # verify the homepage of the site
-    def test_homepage(self):
-
-        # verify the title of the page
-        current_title = self.driver.title
-        expected_title = "Nameere Olive Nives"
-        self.assertEqual(expected_title, current_title, f"Got wrong title. Current title: {self.driver.title}")
-
+    # verify the projects are displayed on the projects page
+    def test_projects_page(self):
         # verify the url of the page
         current_url = self.driver.current_url
         expected_url = "https://nameere-olive-nives.netlify.app/"
         self.assertEqual(expected_url, current_url, f"Got wrong site url. Current url: {current_url}")
 
-        # verify the content of the bio section of the home-page is displayed
-        bio_link_locator = "#root > div > div.container-div > div.bio-div > p"
-        bio_element = self.driver.find_element(By.CSS_SELECTOR, bio_link_locator)
-        # wait a few seconds before checking since bio api takes long to load on browser
-        time.sleep(2)
-        self.assertTrue(bio_element.is_displayed())
+        # navigate to the projects page
+        projects_button_locator = '//*[@id="responsive-navbar-nav"]/div/a[2]'
+        projects_element = self.driver.find_element(By.XPATH, projects_button_locator)
+        projects_element.click()
 
+        # verify the url of the current page
+        expected_url = "https://nameere-olive-nives.netlify.app/projects"
+        current_url = self.driver.current_url
+        self.assertEqual(expected_url, current_url, f"Got wrong site url. Current url : {current_url}")
+
+        # identify project element by xpath
+        # verify its displayed
         # pdb.set_trace()
 
     def tearDown(self) -> None:
